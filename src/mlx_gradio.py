920447d92a91b4dd397d0e11b7fb2ef316636828
David Hundley <david.k.hundley@gmail.com>
2024-03-09 11:26:45 -0600

    Starting an idealized schema

--- src/mlx_gradio.py
+++ src/mlx_gradio.py
@@ -5,12 +5,35 @@ import gradio as gr
 
 
 
+def greet(history, input):
+    return history + [(input, "Hello, " + input)]
+
+def vote(data: gr.LikeData):
+    if data.liked:
+        print("You upvoted this response: " + data.value)
+    else:
+        print("You downvoted this response: " + data.value)
+
+
+
+    
+
 ## GRADIO UI LAYOUT & FUNCTIONALITY
 ## ---------------------------------------------------------------------------------------------------------------------
 # Defining the building blocks that represent the form and function of the Gradio UI
-with gr.Blocks() as chat_ui:
+with gr.Blocks() as demo:
+
+    chatbot = gr.Chatbot()
+    textbox = gr.Textbox()
+    textbox.submit(greet, [chatbot, textbox], [chatbot])
+    chatbot.like(vote, None, None)
+
+    with gr.Row():
+        text = gr.Textbox(label = 'David',
+                          interactive = False)
+        
+    examples = gr.Examples(examples = ['David', 'Hundley'])
     
-    pass
 
 
 
@@ -19,4 +42,4 @@ with gr.Blocks() as chat_ui:
 if __name__ == "__main__":
 
     # Launching the Gradio Chatbot
-    chat_ui.launch(share = True)
+    demo.launch()
