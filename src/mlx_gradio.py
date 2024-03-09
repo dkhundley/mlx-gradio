db2ae0e2c919990095865837a9bb19d1ba71c5ff
David Hundley <david.k.hundley@gmail.com>
2024-03-09 09:43:10 -0600

    Starting file for Gradio UI

---
+++ src/mlx_gradio.py
@@ -0,0 +1,22 @@
+# Importing the necessary Python libraries
+import os
+import json
+import gradio as gr
+
+
+
+## GRADIO UI LAYOUT & FUNCTIONALITY
+## ---------------------------------------------------------------------------------------------------------------------
+# Defining the building blocks that represent the form and function of the Gradio UI
+with gr.Blocks() as chat_ui:
+    
+    pass
+
+
+
+## SCRIPT INVOCATION
+## ---------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+
+    # Launching the Gradio Chatbot
+    chat_ui.launch(share = True)
--- src/test.py
+++ src/test.py
@@ -1,5 +1,31 @@
-from mlx_lm import load, generate
+# Importing the necessary Python libraries
+import os
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model, tokenizer = load('mistralai/Mistral-7B-Instruct-v0.1')
-response = generate(model, tokenizer, prompt = 'Write a haiku about flowers', verbose = True)
-print(response)
+# Setting constant values to represent model name and directory
+MODEL_NAME = 'mistralai/Mixtral-8x7B-Instruct-v0.1'
+BASE_DIRECTORY = '../models/'
+
+# Setting the full model directory path
+model_directory = f'{BASE_DIRECTORY}{MODEL_NAME}'
+
+
+# Checking to see if the directory has already been created
+if os.path.exists(model_directory):
+
+    # Loading the tokenizer and model from local file
+    tokenizer = AutoTokenizer.from_pretrained(model_directory)
+    model = AutoModelForCausalLM.from_pretrained(model_directory)
+
+else:
+
+    # Creating the new model directory
+    os.makedirs(model_directory)
+
+    # Downloading the tokenizer and model from HuggingFace
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
+
+    # Saving the tokenizer and model to model directory
+    tokenizer.save_pretrained(save_directory = model_directory)
+    model.save_pretrained(save_directory = model_directory)
