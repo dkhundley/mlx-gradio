a76ed5d6d23c8b261a46e510a1178bf51b67e88f
David Hundley <david.k.hundley@gmail.com>
2024-04-12 21:08:39 -0500

    Starting on the UI and backbone for the backend

--- src/mlx_gradio.py
+++ src/mlx_gradio.py
@@ -1,7 +1,31 @@
 # Importing the necessary Python libraries
 import os
+import yaml
 import json
+import pandas as pd
 import gradio as gr
+from utils import *
+
+test_data = {
+    'Chat History': [
+        'This is my first chat',
+        'Here is another one',
+        'And a third!'
+    ],
+    # 'Val': [
+    #     '1',
+    #     '2',
+    #     '4'
+    # ]
+}
+
+df = pd.DataFrame(test_data)
+
+## API INSTANTIATION
+## ---------------------------------------------------------------------------------------------------------------------
+# Loading the API key and organization ID from file (NOT pushed to GitHub)
+with open('../sensitive/api-keys.yaml') as f:
+    keys_yaml = yaml.safe_load(f)
 
 
 
@@ -15,24 +39,40 @@ def vote(data: gr.LikeData):
         print("You downvoted this response: " + data.value)
 
 
+def load_historical_chat(row):
+    print(row)
+    pass
+
+
 
     
 
 ## GRADIO UI LAYOUT & FUNCTIONALITY
 ## ---------------------------------------------------------------------------------------------------------------------
 # Defining the building blocks that represent the form and function of the Gradio UI
-with gr.Blocks() as demo:
+with gr.Blocks(theme = gr.themes.Base()) as demo:
 
-    chatbot = gr.Chatbot()
-    textbox = gr.Textbox()
-    textbox.submit(greet, [chatbot, textbox], [chatbot])
-    chatbot.like(vote, None, None)
+    with gr.Tab('Chat'):
+        with gr.Row():
+            with gr.Column(scale = 1):
+                
+                new_chat_button = gr.Button()
 
-    with gr.Row():
-        text = gr.Textbox(label = 'David',
-                          interactive = False)
-        
-    examples = gr.Examples(examples = ['David', 'Hundley'])
+                chat_history = gr.DataFrame(
+                    value = df
+                )
+
+            with gr.Column(scale = 4):
+                chatbot = gr.Chatbot(show_label = False)
+                textbox = gr.Textbox()
+                textbox.submit(greet, [chatbot, textbox], [chatbot])
+                chatbot.like(vote, None, None)
+
+    with gr.Tab('Settings'):
+        gr.Textbox()
+
+
+    chat_history.select(fn = load_historical_chat, inputs = None, outputs = None)
     
 
 
---
+++ src/utils.py
@@ -0,0 +1,52 @@
+def generate_summary_title():
+    '''
+    Generates a summary title based on the interaction between the user and the AI
+
+    Inputs:
+        - ?
+
+    Returns:
+        - ?
+    '''
+    pass
+
+
+
+def load_chat_history():
+    '''
+    Loads chat history from file
+
+    Inputs:
+        - ?
+    
+    Returns:
+        - ?
+    '''
+    pass
+
+
+
+def save_chat_history():
+    '''
+    Saves chat history to file
+
+    Inputs:
+        - ?
+
+    Returns:
+        - ?
+    '''
+    pass
+
+
+
+def start_new_chat():
+    '''
+    Starts a new chat interaction
+
+    Inputs:
+        - ?
+
+    Returns:
+        - ?
+    '''
